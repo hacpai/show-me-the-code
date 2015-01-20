6b96a418d9763dbff9c9737f82d9a2c6948ae7c2
arcticlion <david.malan@aol.com>
2015-01-20 23:13:52 +0800

    Finish Problem 0003

--- Python/0003/main.py
+++ Python/0003/main.py
@@ -1,39 +1,13 @@
-def word_max_len ( my_dict ):
-    max_len = 1
-    my_dict = unicode( my_dict, 'utf-8' )
-    my_dict = my_dict.split(" ")
-    for word in my_dict:
-        if max_len < len( word ):
-            max_len = len( word )
-    return max_len
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  9 11:10:56 2014
 
-def rmm_word_seg( sen, max_len, my_dict ):
-    words = []
-    sen = unicode( sen, 'utf-8' )
-    end = len(sen)
-    my_dict = unicode( my_dict, 'utf-8' )
-    my_dict = my_dict.split(" ")
-    while end > 0:
-        for begin in range ( end - max_len, end ):
-            if sen[begin:end] in my_dict:
-                words.insert( 0, sen[begin:end] )
-                break
-        else:
-            words.insert( 0, sen[begin:end] )
+@author: MasterMac
+"""
+import math
+a = float(raw_input())
+b = float(raw_input())
+c = float(raw_input())
 
-        end = begin 
-
-    return words
-
-my_dict = raw_input()
-for sen in iter( raw_input, "" ):
-    max_len = word_max_len( my_dict )
-    words = rmm_word_seg( sen, max_len, my_dict )
-#    delimiter = " "
-#    print delimiter.join(words).lstrip().encode('utf_8')
-    for k,word in enumerate(words):
-        if(k==(len(words)-1)):
-            print word.encode('utf-8')
-        else:
-            print word.encode('utf-8'),
-
+cosC = (a**2 + b**2 - c**2)/(2.0 * a * b)
+print round(math.degrees(math.acos(cosC)), 1)
