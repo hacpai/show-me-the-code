1a93d394aa1156c1d2a509f2fb88d8f8cf4452ae
arcticlion <david.malan@aol.com>
2015-02-06 14:59:11 +0800

    Finish Problem 0039

---
+++ Python/0039/main.py
@@ -0,0 +1,57 @@
+import time
+
+
+def make_word_list_v1():
+    """Reads hte file words.txt and builds a list with one element per word.
+    using append method
+    """
+    t = []
+    fin = open( 'words.txt', 'r' )
+    for line in fin:
+        word = line.strip()
+        t.append( word )
+
+    return t
+
+
+def make_word_list_v2():
+    """Reads hte file words.txt and builds a list with one element per word.
+    using idiom t = t + [x]
+    """
+    t = []
+    fin = open( 'words.txt', 'r' )
+    for line in fin:
+        word = line.strip()
+        t = t + [word]
+
+    return t
+
+
+def make_word_list_v2_1():
+    """Reads hte file words.txt and builds a list with one element per word.
+    using idiom t = t + [x]
+    """
+    t = []
+    fin = open( 'words.txt', 'r' )
+    for line in fin:
+        word = line.strip()
+        t += [word]
+
+    return t
+
+
+def measure_elapsed_time( f ):
+    """use the time module to measure function elapsed time.
+
+    f: function
+    """
+    start = time.time()
+
+    f()
+
+    return time.time() - start
+
+
+print measure_elapsed_time( make_word_list_v1 ), 'seconds'
+print measure_elapsed_time( make_word_list_v2 ), 'seconds'
+print measure_elapsed_time( make_word_list_v2_1 ), 'seconds'
