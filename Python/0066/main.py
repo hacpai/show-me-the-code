7503eec179620920d0a47594f0cd62b38c475028
arcticlion <david.malan@aol.com>
2015-02-21 22:59:32 +0800

    :good: Finish Problem 0066

--- Python/0066/main.py
+++ Python/0066/main.py
@@ -1,46 +1,57 @@
 from Card import *
 
 
+class Hist(dict):
+
+    def __init__(self, seq=None):
+        if seq == None:
+            seq = []
+        for x in seq:
+            self.count(x)
+
+    def count(self, x):
+        """Counts a number of x.
+        """
+        self[x] = self.get(x, 0) + 1
+
 class PokerHand(Hand):
 
-    def make_hist(self):
-        """Builds a histogram of suits and ranks that appear in the hand.
+    labels = ['straightflush', 'fourkind', 'fullhouse', 'flush',
+            'straight', 'threekind', 'twopair', 'pair', 'highcard']
 
-        Stores the result in attribute suits and ranks.
+    def make_hist(self):
         """
-        self.suits = {}
-        self.ranks = {}
+        Builds a histogram of suits and ranks that appear in the hand.
+
+        Creates attributes:
+            suits: a histogram of the suits in the hand.
+            ranks: a histogram of the ranks.
+            sets: a list of the ranks values.
+        """
+        self.ranks = Hist()
+        self.suits = Hist()
         for card in self.cards:
-            self.suits[card.suit] = self.suits.get(card.suit, 0) + 1
-            self.ranks[card.rank] = self.ranks.get(card.rank, 0) + 1
-        self.rank_list = self.ranks.keys()
-        self.rank_list.sort()
-        if self.rank_list[0] == 1:
-            self.rank_list.append(14)
+            self.ranks.count(card.rank)
+            self.suits.count(card.suit)
+
+        self.sets = self.ranks.values()
+        self.sets.sort(reverse=True)
 
     def has_same_suit(self, num):
         """Return True if the number of hand has same suits, False otherwise.
         """
-        self.make_hist()
         for val in self.suits.values():
             if val >= num:
                 return True
         return False
 
-    def has_same_rank(self, same_rank=2, num=1):
+    def check_sets(self, *kinds):
         """Return True if the number of hand has same ranks, False otherwise.
         """
-        self.make_hist()
-        self.rank_hist = {}
-        for kind in self.ranks.values():
-            self.rank_hist[kind] = self.rank_hist.get(kind, 0) + 1
-
-        try:
-            if self.rank_hist[same_rank] == num:
-                return True
-        except KeyError:
-            return False
-        return False
+        for kind, ranks_val in zip(kinds, self.sets):
+            if kind > ranks_val:
+                return False
+        return True
 
     def has_flush(self):
         """Returns True if the hand has a flush, False otherwise.
@@ -54,108 +65,85 @@ class PokerHand(Hand):
 
         Note that this works correctly for hands with more than 2 cards.
         """
-        return self.has_same_rank(2, 1)
+        return self.check_sets(2)
 
     def has_twopair(self):
         """Returns True if the hand has two pair, False otherwise.
 
         Note that this works correctly for hands with more than 4 cards.
         """
-        return self.has_same_rank(2, 2)
+        return self.check_sets(2, 2)
 
-    def has_three_of_a_kind(self):
+    def has_threekind(self):
         """Return True if the hand has three of a kind, False otherwise.
 
         Note that this works correctly for hands with more than 3 cards.
         """
-        return self.has_same_rank(3, 1)
+        return self.check_sets(3)
 
     def has_straight(self):
         """Return True if the hand has five cards with ranks in sequence.
 
         Note that this works correctly for hands with more than 5 cards.
         """
-        self.make_hist()
-        count = 1
-        for i in range(len(self.rank_list)-1):
-            if self.rank_list[i] + 1 == self.rank_list[i+1]:
+        ranks = self.ranks.copy()
+        ranks[14] = ranks.get(1, 0)
+        return self.check_row(ranks)
+
+    def check_row(self, ranks):
+        count = 0
+        for n in range(1, 15):
+            if n in ranks:
                 count += 1
                 if count >= 5:
                     return True
             else:
-                count = 1
+                count = 0
         return False
 
-    def has_full_house(self):
+    def has_fullhouse(self):
         """Return True the hand has three cards with one rank, two cards with another.
 
         Note that this works correctly for hands with more than 5 cards.
         """
-        return self.has_three_of_a_kind() and self.has_pair()
+        return self.check_sets(3,2)
 
-    def has_four_of_a_kind(self):
+    def has_fourkind(self):
         """Return True if the hand has four of a kind, False otherwise.
 
         Note that this works correctly for hands with more than 4 cards.
         """
-        return self.has_same_rank(4, 1)
+        return self.check_sets(4)
 
-    def has_straight_flush(self):
+    def has_straightflush(self):
         """Return True if the hand has five cards in sequence and with the same suit.
-        
+
         Note that this works correctly for hands with more than 5 cards.
         """
-        return self.has_flush() and self.has_straight()
+        d = Hist()
+        for c in self.cards:
+            d.setdefault(c.suit, PokerHand()).add_card(c)
+        for hand in d.itervalues():
+            if len(hand.cards) < 5:
+                continue
+            hand.make_hist()
+            if hand.has_straight():
+                return True
+        return False
+
+    def has_highcard(self):
+        """Return True if this hand has a high card."""
+        return len(self.cards)
 
     def classify(self):
         """Figures out the highest-value classification for a hand and
         sets the label attribute accordingly.
         """
-        if self.has_straight_flush():
-            self.label = 'straight flush'
-            return
-        if self.has_four_of_a_kind():
-            self.label = 'four of a kind'
-            return
-        if self.has_full_house():
-            self.label = 'full house'
-            return
-        if self.has_flush():
-            self.label = 'flush'
-            return
-        if self.has_straight():
-            self.label = 'straight'
-            return
-        if self.has_three_of_a_kind():
-            self.label = 'three of a kind'
-            return
-        if self.has_twopair():
-            self.label = 'two pair'
-            return
-        if self.has_pair():
-            self.label = 'pair'
-            return
-        self.label = 'no pair'
-        return
-
-def main():
-    """Estimate the probabilities of the various hands.
-
-    shuffles a deck of cards, divides it into hands, classify the hands and counts the number of times various classifications appear.
-    """
-    categories = {}
-    for i in range(100000):
-        deck = Deck()
-        deck.shuffle()
-        hand = PokerHand()
-        deck.move_cards(hand, 5)
-        hand.sort()
-        hand.classify()
-        categories[hand.label] = categories.get(hand.label, 0 ) + 1
-
-    for categorie, freq in categories.iteritems():
-        print categorie, 100.0 * freq / 100000.0, '%'
-
+        for label in PokerHand.labels:
+            f = getattr(self, 'has_'+label)
+            if f():
+                self.label = label
+                return
 
 def print_attributes(obj):
     for attr in obj.__dict__:
@@ -163,4 +151,21 @@ def print_attributes(obj):
 
 
 if __name__ == '__main__':
-    main()
+    kinds = Hist()
+    for k in range(10000):
+        if k % 1000 == 0:
+            print k
+        for i in range(10):
+            deck = Deck()
+            deck.shuffle()
+            hand = PokerHand()
+            deck.move_cards(hand, 5)
+            hand.make_hist()
+            hand.classify()
+            kinds.count(hand.label)
+
+    for label in PokerHand.labels:
+        try:
+            print label, 100.00 * kinds[label]/100000, '%'
+        except KeyError:
+            continue
