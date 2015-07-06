5a062b58d9894408cde8f03fb977596f1ca9274c
henry-zhuang <mountainsea@outlook.com>
2015-07-06 15:53:36 +0800

    :beer: Refine Problem 0008

--- Go/0008/http-handlers.go
+++ Go/0008/http-handlers.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 )
 
 type String string
@@ -28,14 +29,28 @@ func (h Hello) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Welcome")
 }
 
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fmt.Println(r.Form)
+	fmt.Println("paht", r.URL.Path)
+	fmt.Println("Scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("key", k)
+		fmt.Println("val:", strings.Join(v, ""))
+	}
+	fmt.Fprintf(w, "hello david")
+}
+
 func main() {
 	//http.Handle("/", Hello{})
 	http.Handle("/string", String("I'm a frayed knot."))
 	http.Handle("/struct", &Struct{"Hello", ":", "Gophers!"})
 	//err := http.ListenAndServe("localhost:4000", nil)
 
-	var h Hello
-	err := http.ListenAndServe("localhost:4000", h)
+	http.HandleFunc("/hello", sayhelloName)
+	//var h Hello
+	err := http.ListenAndServe("localhost:9090", nil)
 
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err.Error())
